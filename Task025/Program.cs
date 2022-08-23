a9fd45b612612ed6dcbab05974ff5cc0d75cc28f
«evgeny» <solov-ghenya195@mail.ru>
2022-08-23 17:30:27 +0300

    add Task027

--- Task025/Program.cs
+++ Task025/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+int ExponentXY(int x, int y)
+{
+    int sum = 1;
+    for(int i = 1; i <= y; i++)
+    {
+        int a = 0;
+        a = sum*x;
+        sum = a;
+    }
+    return sum;
+}
+
+
+Console.WriteLine(ExponentXY(x, y));
---
+++ Task027/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите целое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int Sum (int x)
+{
+    string y = x.ToString();
+    int[] array = new int[y.Length];
+    int sum = 0;
+    for(int i = 0; i < y.Length; i++)
+    { 
+        array[i] = int.Parse(y[i]+ "");
+        sum = sum + array[i];
+    }
+    return sum;
+}
+Console.WriteLine(Sum(x));
